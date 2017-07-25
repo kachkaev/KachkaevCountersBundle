b7c3ec7745ee9abc3db5c567ef5dd5638990da3c
Alexander Kachkaev <alexander@kachkaev.ru>
2017-07-25 08:41:37 +0100

    Correctly refer to global classes

--- Twig/CountersExtension.php
+++ Twig/CountersExtension.php
@@ -2,11 +2,7 @@
 namespace Kachkaev\CountersBundle\Twig;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-use Twig_Extension;
-use Twig_Filter_Method;
-use Twig_Function_Method;
-
-class CountersExtension extends Twig_Extension
+class CountersExtension extends \Twig_Extension
 {
     protected $container;
 
@@ -22,13 +18,13 @@ class CountersExtension extends Twig_Extension
         $mappings = array('getCounterId' => 'getCounterId', 'countersDisabled' => 'countersDisabled',);
 
         foreach ($mappings as $twigFunction => $method) {
-            $functions[$twigFunction] = new Twig_Function_Method($this, $method);
+            $functions[$twigFunction] = new \Twig_Function_Method($this, $method);
         }
 
         $safeMappings = array();
 
         foreach ($safeMappings as $twigFunction => $method) {
-            $functions[$twigFunction] = new Twig_Function_Method($this, $method, array('is_safe' => array('html')));
+            $functions[$twigFunction] = new \Twig_Function_Method($this, $method, array('is_safe' => array('html')));
         }
 
         return $functions;
